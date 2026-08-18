b9fdb6f5528b9a0dbee1bd3c473e6919d1d55986
hirdyanshg <hirdyanshgoyal7@gmail.com>
2026-08-18 12:30:53 +0530

    day7

---
+++ EVENTMODULE/Day7.js
@@ -0,0 +1,9 @@
+//create your own server using http module
+const http=require('http');
+const server = http.createServer((req,res)=>{
+    res.write('Hello World');
+    res.end();
+})
+server.listen(3000,()=>{
+    console.log('Server is running on port 3000');
+})
